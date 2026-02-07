31f0031d2926efd22cf1a65e05a55a803f35e5f0
Yasiru Lakruwan <yapa1459@gmail.com>
2026-02-07 21:13:28 +0530

    commit

--- src/controller/todoFormController.java
+++ src/controller/todoFormController.java
@@ -7,17 +7,22 @@ public class todoFormController {
     public AnchorPane root;
 
     public void btnOnClickActionDelete(ActionEvent actionEvent) {
+
     }
 
     public void btnOnClickActionUpdate(ActionEvent actionEvent) {
+
     }
 
     public void btnOnClickActionAddNewToDo(ActionEvent actionEvent) {
+
     }
 
     public void btnOnClickActionAddToList(ActionEvent actionEvent) {
+
     }
 
     public void btnOnClickActionLogOut(ActionEvent actionEvent) {
+
     }
 }
